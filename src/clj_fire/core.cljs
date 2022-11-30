6f5f296c351e6ad9a5991965c99b319720c1185a
mjarus007 <lilitha.potye@lifecheq.co.za>
2022-11-30 05:05:38 +0200

    Remove logging + app state

--- src/clj_fire/core.cljs
+++ src/clj_fire/core.cljs
@@ -1,14 +1,8 @@
 (ns clj-fire.core
   (:require
    [goog.dom :as gdom]
-   [reagent.core :as reagent :refer [atom]]
    [reagent.dom :as rdom]
    [clj-fire.auth.views :as auth.views]))
-
-(println "This text is printed from src/clj_fire/core.cljs. Go ahead and edit it and see reloading in action.")
-
-;; define your app data so that it doesn't get over-written on reload
-(defonce app-state (atom {:text "Hello world!"}))
 
 (defn get-app-element []
   (gdom/getElement "app"))
