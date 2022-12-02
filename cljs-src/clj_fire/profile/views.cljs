937f0c153636e8dee7c78e025a12f25b9ba43e96
mjarus007 <lilitha.potye@lifecheq.co.za>
2022-12-02 07:48:29 +0200

    print out raw user profile

--- cljs-src/clj_fire/profile/views.cljs
+++ cljs-src/clj_fire/profile/views.cljs
@@ -1,10 +1,14 @@
 (ns clj-fire.profile.views
   (:require [clj-fire.auth.views :as auth.views]
-            [clj-fire.views :refer [panels]]))
+            [clj-fire.auth.subs :as auth.subs]
+            [clj-fire.views :refer [panels]]
+            [re-frame.core :as re-frame]))
 
 (defn content []
-  [:section
-   [:h1 "Profile"]])
+  (let [fb-user @(re-frame/subscribe [::auth.subs/fb-user])]
+    [:section
+     [:h1 "Profile"]
+     [:pre fb-user]]))
 
 (defn main-panel []
   [auth.views/auth-wrapper [content]])
