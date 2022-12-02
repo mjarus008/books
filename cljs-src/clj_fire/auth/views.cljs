3d2a5c3950018afa4f2123927055a94288347814
mjarus007 <lilitha.potye@lifecheq.co.za>
2022-12-02 04:39:08 +0200

    refractor: create fb-config

--- cljs-src/clj_fire/auth/views.cljs
+++ cljs-src/clj_fire/auth/views.cljs
@@ -3,6 +3,20 @@
             [clj-fire.firebase :as fb]
             ["firebase/compat/app$default" :as firebase]))
 
+(def ^:private fb-config
+  {:signInOptions
+   [{:provider firebase.auth.EmailAuthProvider.PROVIDER_ID
+     :requireDisplayName false}
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID]
+   :callbacks {:signInSuccessWithAuthResult
+               (fn [auth-result _redirect-url]
+                 (print "auth result: " auth-result)
+                 true)
+               :uiShown
+               (fn []
+                 (print "UI shown."))}
+   :signInSuccessUrl "http://localhost:9500/"})
+
 (defn login-panel []
   (let [id "firebase-login"]
     (reagent/create-class
@@ -10,19 +24,7 @@
       :component-did-mount
       (fn [_this]
         (fb/init-login-panel! 
-         {:id id
-          :signInOptions
-          [{:provider firebase.auth.EmailAuthProvider.PROVIDER_ID
-            :requireDisplayName false}
-           firebase.auth.GoogleAuthProvider.PROVIDER_ID]
-          :callbacks { :signInSuccessWithAuthResult
-           (fn [auth-result redirect-url]
-             (print "auth result: " auth-result)
-             true)
-           :uiShown
-           (fn []
-             (print "UI shown."))}
-          :signInSuccessUrl "http://localhost:9500/"}))
+         (assoc fb-config :id id)))
       :reagent-render
       (fn []
         [:div {:id id}])})))
