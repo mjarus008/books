d9b9979a3463171fe8f681e64a3467c6283cc67d
mjarus007 <lilitha.potye@lifecheq.co.za>
2022-12-02 04:16:50 +0200

    Remove test text

--- cljs-src/clj_fire/home/views.cljs
+++ cljs-src/clj_fire/home/views.cljs
@@ -6,7 +6,7 @@
   [:div.intro
    [:h1.intro--header "Books"]
    [:p.intro--content "We have books for you to read, study with others online and benefit from others' notes."]
-   [:p "Get access to tutors when you get stuck, at a low cost.xyz"]
+   [:p "Get access to tutors when you get stuck, at a low cost."]
    [:br]
    [auth.views/login-panel]])
 
