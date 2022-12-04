bd7ef54ed402b6c1ac9ffa57a2ed1ed9ac9ce923
mjarus007 <lilitha.potye@lifecheq.co.za>
2022-12-04 08:10:24 +0200

    basic print of profile

--- cljs-src/clj_fire/profile/views.cljs
+++ cljs-src/clj_fire/profile/views.cljs
@@ -1,19 +1,21 @@
 (ns clj-fire.profile.views
-  (:require [clj-fire.auth.views :as auth.views]
-            [clj-fire.auth.subs :as auth.subs]
+  (:require [clj-fire.auth.subs :as auth.subs]
+            [clj-fire.auth.views :as auth.views]
+            [clj-fire.utils :as utils]
             [clj-fire.views :refer [panels]]
+            [clojure.pprint :as pretty]
             [re-frame.core :as re-frame]))
 
 (defn content []
   (let [fb-user @(re-frame/subscribe [::auth.subs/fb-user])]
     [:section
      [:h1 "Profile"]
-     [:pre fb-user]]))
+     [:pre.code-block (utils/pretty-str (select-keys fb-user [:display-name :providerData]))]]))
 
 (defn main-panel []
   [auth.views/auth-wrapper [content]])
 
 (defmethod panels :profile
   [_]
-  [main-panel])
+  [main-panel]) 
 
