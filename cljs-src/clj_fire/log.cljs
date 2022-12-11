0697e54322e37f3d10aa00b7b550fd969f38e904
mjarus007 <lilitha.potye@lifecheq.co.za>
2022-12-11 07:24:40 +0200

    Add logging ns

--- cljs-src/clj_fire/log.cljs
+++ cljs-src/clj_fire/log.cljs
@@ -1,7 +1,30 @@
-(ns clj-fire.log)
+(ns clj-fire.log 
+  (:require [re-frame.core :as re-frame]))
 
-(def warn print)
+(def warn prn)
 
-(def error print)
+(def error prn)
 
-(def info print)
+(def info prn)
+
+(def trace prn)
+
+(re-frame/reg-event-fx
+ :log/error
+ (fn [_ [_ args]]
+   {:log/error args}))
+
+(re-frame/reg-fx
+ :log/error
+ (fn [args]
+   (error args)))
+
+(re-frame/reg-event-fx
+ :log/info
+ (fn [_ [_ args]]
+   {:log/info args}))
+
+(re-frame/reg-fx
+ :log/info
+ (fn [args]
+   (info args)))
