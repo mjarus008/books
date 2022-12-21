5292b7e9352c73f7a8a2735c545f1d57aa15154e
mjarus007 <lilitha.potye@lifecheq.co.za>
2022-12-21 06:47:38 +0200

    Add link to chat

--- cljs-src/clj_fire/profile/views.cljs
+++ cljs-src/clj_fire/profile/views.cljs
@@ -2,6 +2,7 @@
   (:require [clj-fire.auth.events :as auth.events]
             [clj-fire.auth.subs :as auth.subs]
             [clj-fire.auth.views :as auth.views]
+            [clj-fire.routes :as routes]
             [clj-fire.utils :as utils]
             [clj-fire.views :refer [panels]]
             [re-frame.core :as re-frame]))
@@ -11,7 +12,9 @@
     [:section
      [:h1 "Profile"]
      [:button.sign-out {:on-click #(re-frame/dispatch [::auth.events/logout])} "logout"]
-     [:pre.code-block (utils/pretty-str (select-keys fb-user [:display-name :providerData]))]]))
+     [:pre.code-block (utils/pretty-str (select-keys fb-user [:display-name :providerData]))]
+     [:br]
+     [:a {:href (routes/form-url :chat)} "go to chat"]]))
 
 (defn main-panel []
   [auth.views/auth-wrapper [content]])
